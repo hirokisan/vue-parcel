e252f15f77bad5c8e2e79599669e1dab460ef499
hirokisan <expsharetoworld@gmail.com>
2018-10-11 21:37:14 +0900

    Use yml

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../css/main.css';
 import Icon from '../img/icon.png';
 import Json from '../json/name.json';
+import Yml  from '../yml/name.yml';
 
 function component() {
   let element = document.createElement('div');
@@ -14,6 +15,7 @@ function component() {
   element.appendChild(myIcon);
 
   console.log(Json);
+  console.log(Yml);
 
 
   return element;
