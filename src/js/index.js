e56fa19c806f09e2f841c7ae476e358ac7f26d8e
hirokisan <expsharetoworld@gmail.com>
2018-10-11 23:24:47 +0900

    Use react

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import '../css/main.css';
 import '../css/main.scss';
+import '../js/react.js';
 import Icon from '../img/icon.png';
 import Json from '../json/name.json';
 import Yml  from '../yml/name.yml';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function component() {
   let element = document.createElement('div');
@@ -23,3 +26,8 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+const Application = () => {
+  return <h1>Hello world!</h1>
+}
+ReactDOM.render(<Application />, document.getElementById('app'));
