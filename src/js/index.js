12e1b767f5f29acbe526289ed6e2a1ee039be826
hirokisan <expsharetoworld@gmail.com>
2018-10-11 21:05:00 +0900

    Use image

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
 import '../css/main.css';
+import Icon from '../img/icon.png';
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = "sample parcel";
   element.classList.add('main');
+
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
 
   return element;
 }
